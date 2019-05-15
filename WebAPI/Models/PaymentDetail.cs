51c7da0bdc5ce1019d4a8f91d508c7d790490642
Adam Clarke <adamclarke88@gmail.com>
2019-05-15 10:15:27 +0100

    initial commit

---
+++ WebAPI/Models/PaymentDetail.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class PaymentDetail
+    {
+        [Key] // setting PMId to primary key
+        public int PMId { get; set; }
+
+        [Required] // making this field required
+        [Column(TypeName ="nvarchar(100)")] // limiting the amount of characters  
+        public string CardOwnerName { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(16)")]
+        public string CardNumber { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(5)")]
+        public string ExpirationDate { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(3)")]
+        public string CVV { get; set; }
+    }
+}
